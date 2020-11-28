e1a990f0b200cc8e14c92049d43476e8feff1570
jr1221 <53871299+jr1221@users.noreply.github.com>
2020-11-28 10:58:08 -0500

    Create apiKEY.java

---
+++ src/apiKEY.java
@@ -0,0 +1,15 @@
+import java.util.Base64;
+
+public class apiKey {
+    String encodeBytes() {
+
+
+        String Username = "REPLACE THESE WORDS WITH USERNAME AND LEAVE QUOTATION MARKS";
+        String Authorization_Token = "REPLACE THESE WORDS WITH AUTHORIZATION TOKEN AND LEAVE QUOTATION MARKS";
+
+
+
+        String encodeBytes = Base64.getEncoder().encodeToString((Username + ":" + Authorization_Token).getBytes());
+        return encodeBytes;
+    }
+}
