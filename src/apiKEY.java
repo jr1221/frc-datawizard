68fefb98b0cab465be92c7b8276046055cbbd42b
jr1221 <53871299+jr1221@users.noreply.github.com>
2020-12-19 18:00:09 -0500

    Update apiKEY.java
    Manually update for static call

--- src/apiKEY.java
+++ src/apiKEY.java
@@ -1,15 +1,5 @@
-import java.util.Base64;
 
 public class apiKEY {
-    String encodeBytes() {
-
-
-        String Username = "REPLACE THESE WORDS WITH USERNAME AND LEAVE QUOTATION MARKS";
-        String Authorization_Token = "REPLACE THESE WORDS WITH AUTHORIZATION TOKEN AND LEAVE QUOTATION MARKS";
-
-
-
-        String encodeBytes = Base64.getEncoder().encodeToString((Username + ":" + Authorization_Token).getBytes());
-        return encodeBytes;
-    }
+     static final String Username = "REPLACE THESE WORDS WITH YOUR USERNAME";
+     static final String Authorization_Token = "REPLACE THESE WORDS WITH YOUR AUTH TOKEN";
 }
