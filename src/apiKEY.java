97f8169c9016fceaa490d8b4513d90e8dff83046
Jack R <rubacha dot jack03 at gmail dot com>
2020-12-20 22:31:00 -0500

    Remove apiKEY file.

--- src/apiKEY.java
+++
@@ -1,5 +0,0 @@
-
-public class apiKEY {
-     static final String Username = "REPLACE THESE WORDS WITH YOUR USERNAME";
-     static final String Authorization_Token = "REPLACE THESE WORDS WITH YOUR AUTH TOKEN";
-}
